9dc4de2c2ee59829e0096dba4aa2f74e4d23cb69
mascarenhasluiz <luizc.mascarenhas@outlook.com>
2024-06-22 08:32:01 +0900

    Nova versão

---
+++ www/lib/plugins.js
@@ -0,0 +1,19 @@
+$(document).on("click","#camera",function(){
+    navigator.camera.getPicture(onSuccess,onFail,{
+        quality:50,destinationType: Camera.DestinationType.FILE_URI,
+
+    correctOrientation: true,
+    saveToPhotoAlbum: true
+
+});
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+});
